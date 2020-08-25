36f3342524e8e1092d08566021fddcd5e09303be
jrhunger <1101010@gmail.com>
2020-08-24 21:47:32 -0400

    x/y position logic

--- cart.asm
+++ cart.asm
@@ -3,11 +3,40 @@
 	include "vcs.h"
 	include "macro.h"
 
+;;;; start constant declarations
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+P0HEIGHT equ 9
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; end constant declarations
+
+;;; $80 to $FF for variables, minus some at end if using stack
+	seg.u variables
+	org $80
+;;;;  start variable declarations
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+P0x	byte	; P0 x
+P0y	byte	; P0 y
+P0spritePtr	ds	; y-adjusted sprite pointer
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;  end variables
+
+;;; Begin code segment in ROM at $F000
 	seg code
 	org $F000
 
 Start:
 	CLEAN_START
+
+;;;;  start variable initialization
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	lda #50
+	sta P0x
+	sta P0y
+;;; Set high byte of P0spritePtr (low byte updated per frame)
+	lda #>P0bitmap
+	sta P0spritePtr+1
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;  end variable initialization
 
 StartFrame:
 	lda #2
@@ -27,25 +56,54 @@ StartFrame:
 
 ;;;;  start game vblank logic
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; P0 horizontal position
+	ldx #0
+	lda P0x
+	jsr PosObject
+;;; P0 y pointer
+	lda #<P0bitmap+P0HEIGHT
+	sec
+	sbc P0y
+	sta P0spritePtr
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;  end game vblank logic
 
 ;;;; Wait for rest of VBLANK
-VblankWaitLoop
+.VblankWaitLoop
 	lda INTIM 	; load timer interrupt
-	bne	VblankWaitLoop
+	bne	.VblankWaitLoop
 	sta WSYNC 	; wait for next wsync
 	sta VBLANK	; turn off VBlank
 
+;;;; kernel (192 visible scan lines)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;; 192 visible scan lines
-	ldx #192	; counter
-LoopVisible:
-	stx COLUBK	; set bg color to loop var
+	ldy #192	; counter
+.LoopVisible:
+;;; for rainbow background
+	sty COLUBK	; set bg color to loop var
+
+;;; draw P0
+	sec	; 2 set carry
+	tya	; 2
+	sbc P0y	; 3
+	adc P0HEIGHT	; 2
+	bcs .DrawP0
+
+	nop	; 2
+	nop	; 2
+	sec	; 2
+	bcs .NoDrawP0	; 3
+.DrawP0
+	lda (P0spritePtr),Y	; 5
+	sta GRP0	; 3
+.NoDrawP0
+
 	sta WSYNC	; wait for next scanline
-	dex	; x--
-	bne LoopVisible	; go back until x = 0
+	dey	; y--
+	bne .LoopVisible	; go back until x = 0
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; end kernel
 
 ;;;; set timer for OVERSCAN
 	lda #2
@@ -62,14 +120,101 @@ LoopVisible:
 
 
 ;;;; Wait for rest of OVERSCAN
-OverscanWaitLoop:
+.OverscanWaitLoop:
 	lda INTIM
-	bne OverscanWaitLoop
+	bne .OverscanWaitLoop
 	lda #2
 	sta WSYNC
 
 ;;; new frame
 	jmp StartFrame
+
+;;;;   start subroutines
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;; PosObject from https://www.biglist.com/lists/stella/archives/200403/msg00260.html
+; Positions an object horizontally
+; Inputs: A = Desired position.
+; X = Desired object to be positioned (0-5). *jh* (P0, P1, M0, M1, Ball)
+; scanlines: If control comes on or before cycle 73 then 1 scanline is consumed.
+; If control comes after cycle 73 then 2 scanlines are consumed.
+; Outputs: X = unchanged
+; A = Fine Adjustment value.
+; Y = the "remainder" of the division by 15 minus an additional 15.
+; control is returned on cycle 6 of the next scanline.
+PosObject SUBROUTINE
+
+	STA WSYNC ; 00 Sync to start of scanline.
+	SEC ; 02 Set the carry flag so no borrow will be applied during the division.
+.divideby15
+	SBC #15 ; 04 ; Waste the necessary amount of time dividing X-pos by 15!
+	BCS .divideby15 ; 06/07 - 11/16/21/26/31/36/41/46/51/56/61/66
+
+	TAY ; 08 ; At this point the value in A is -1 to -15. In this code I use a table
+; to quickly convert that value to the fine adjust value needed.
+	LDA fineAdjustTable,Y ; 13 -> Consume 5 cycles by guaranteeing we cross a page boundary
+; In your own code you may wish to consume only 4.
+	STA HMP0,X ; 17 Store the fine adjustment value.
+	STA RESP0,X ; 21/ 26/31/36/41/46/51/56/61/66/71 - Set the rough position.
+
+	STA WSYNC
+	RTS
+
+;;; end PosObject from https://www.biglist.com/lists/stella/archives/200403/msg00260.html
+;;; (see link for alternate way without lookup table)
+	
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;   end subroutines
+
+;;;;  start ROM lookup tables
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+P0bitmap:
+	byte #%00000000
+	byte #%00101001
+	byte #%01101001
+	byte #%10101001
+	byte #%00101001
+	byte #%00101111
+	byte #%00101001
+	byte #%00101001
+	byte #%11101001
+
+P0color:
+	byte #$00
+	byte #$00
+	byte #$00
+	byte #$00
+	byte #$00
+	byte #$00
+	byte #$00
+	byte #$00
+	byte #$00
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;  end ROM lookup tables
+
+;;; fine adjustment for PosObject
+;;; some explanation on "negative index" is here: 
+;;; - https://www.randomterrain.com/atari-2600-memories-tutorial-andrew-davie-24.html
+
+fineAdjustBegin
+	DC.B %01110000; Left 7 
+	DC.B %01100000; Left 6
+	DC.B %01010000; Left 5
+	DC.B %01000000; Left 4
+	DC.B %00110000; Left 3
+	DC.B %00100000; Left 2
+	DC.B %00010000; Left 1
+	DC.B %00000000; No movement.
+	DC.B %11110000; Right 1
+	DC.B %11100000; Right 2
+	DC.B %11010000; Right 3
+	DC.B %11000000; Right 4
+	DC.B %10110000; Right 5
+	DC.B %10100000; Right 6
+	DC.B %10010000; Right 7
+fineAdjustTable EQU fineAdjustBegin - %11110001 ; NOTE: %11110001 = -15
 
 ;;; Complete to 4kB
 	org $FFFC
