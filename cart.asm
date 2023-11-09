32b096a425c8115b7550956bf138b8d998d23448
jrhunger <1101010@gmail.com>
2023-11-08 20:53:32 -0500

    icon and reaction

--- cart.asm
+++ cart.asm
@@ -14,9 +14,13 @@ P0HEIGHT equ 9
 	org $80
 ;;;;  start variable declarations
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-P0x	byte	; P0 x
-P0y	byte	; P0 y
-P0spritePtr	ds	; y-adjusted sprite pointer
+P0x	           byte ; (80) P0 x
+P0y	           byte ; (81) P0 y
+P0spritePtr	   byte ; (82) y-adjusted sprite pointer
+P0spriteHi	   byte ; (83) MSB of sprite pointer
+P0reactionTime byte ; (84) P0 reaction budget 
+P0score        byte ; (85) P0 score
+P0bitmap       byte ; (86) P0bitmap (without screen-draw offset)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;  end variables
 
@@ -33,8 +37,11 @@ Start:
 	sta P0x
 	sta P0y
 ;;; Set high byte of P0spritePtr (low byte updated per frame)
-	lda #>P0bitmap
-	sta P0spritePtr+1
+	lda #>BitmapTable
+	sta P0spriteHi
+;;; Set initial P0bitmap
+	lda #<Ubitmap
+	sta P0bitmap
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;  end variable initialization
 
@@ -56,15 +63,68 @@ StartFrame:
 
 ;;;;  start game vblank logic
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;; skip input detection if sprite is null
+    ldx #<NullBitmap
+	cpx P0bitmap
+	beq EndP0Input
+;;; sprite is active, consume time (?? maybe move lower)
+    dec P0reactionTime
+
+;;; check input signals
+CheckP0Up:
+	lda #%00010000
+	bit SWCHA
+	bne CheckP0Down
+	ldx #<Ubitmap
+CheckP0Down:
+	lda #%00100000
+	bit SWCHA
+	bne CheckP0Right
+	ldx #<Dbitmap
+CheckP0Right:
+	lda #%10000000
+	bit SWCHA
+	bne CheckP0Left
+	ldx #<Rbitmap
+CheckP0Left:
+	lda #%01000000
+	bit SWCHA
+	bne EndP0InputCheck
+	ldx #<Lbitmap
+EndP0InputCheck:
+
+;;; do input-related processing
+CheckInputCorrect:
+    cpx #<NullBitmap   ; if x hasn't changed
+	beq EndP0Input	   ; we didn't get input
+	cpx P0bitmap    ; check if input matches icon
+	bne P0Incorrect
+P0Correct:
+    inc P0score        ; score 1
+	lda #<NullBitmap   ; set bitmap to null
+	sta P0bitmap
+    jmp EndP0Input
+P0Incorrect:
+	lda #<Xbitmap      ; set bitmap to X
+	sta P0bitmap
+;;; end of input processing	
+EndP0Input:
+
 ;;; P0 horizontal position
 	ldx #0
 	lda P0x
 	jsr PosObject
-;;; P0 y pointer
-	lda #<P0bitmap+P0HEIGHT
-	sec
-	sbc P0y
-	sta P0spritePtr
+;;; P0 vertical position
+    lda P0bitmap           ; bitmap base
+	clc                    ; clear carry for add
+	adc #P0HEIGHT-1        ; bitmap high end
+	sec                    ; set carry for subtract
+	sbc P0y                ; offset by P0y for draw logic
+	sta P0spritePtr        ; store in sprite pointer
+	lda #>BitmapTable      ; load 2nd byte of bitmap table
+	sbc #0                 ; subtract 0 (decrements if carry is clear from previous)
+	sta P0spritePtr+1     ; store in high byte of sprite pointer
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;  end game vblank logic
@@ -169,16 +229,73 @@ PosObject SUBROUTINE
 
 ;;;;  start ROM lookup tables
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-P0bitmap:
+    align 256
+BitmapTable:
+Lbitmap:
 	byte #%00000000
-	byte #%00101001
-	byte #%01101001
-	byte #%10101001
-	byte #%00101001
-	byte #%00101111
-	byte #%00101001
-	byte #%00101001
-	byte #%11101001
+	byte #%00010000
+	byte #%00110000
+	byte #%01111111
+	byte #%11111111
+	byte #%01111111
+	byte #%00110000
+	byte #%00010000
+	byte #%00000000
+
+Rbitmap:
+	byte #%00000000
+	byte #%00001000
+	byte #%00001100
+	byte #%11111110
+	byte #%11111111
+	byte #%11111110
+	byte #%00001100
+	byte #%00001000
+	byte #%00000000
+
+Dbitmap:
+	byte #%00000000
+	byte #%00010000
+	byte #%00111000
+	byte #%01111100
+	byte #%11111110
+	byte #%00111000
+	byte #%00111000
+	byte #%00111000
+	byte #%00000000
+
+Ubitmap:
+	byte #%00000000
+	byte #%00111000
+	byte #%00111000
+	byte #%00111000
+	byte #%11111110
+	byte #%01111100
+	byte #%00111000
+	byte #%00010000
+	byte #%00000000
+
+Xbitmap:
+	byte #%00000000
+	byte #%10000010
+	byte #%01000100
+	byte #%00101000
+	byte #%00010000
+	byte #%00101000
+	byte #%01000100
+	byte #%10000010
+	byte #%00000000
+
+NullBitmap:
+	byte #%00000000
+	byte #%00000000
+	byte #%00000000
+	byte #%00000000
+	byte #%00000000
+	byte #%00000000
+	byte #%00000000
+	byte #%00000000
+	byte #%00000000
 
 P0color:
 	byte #$00
@@ -190,6 +307,14 @@ P0color:
 	byte #$00
 	byte #$00
 	byte #$00
+
+P0Index:
+    byte #<Lbitmap
+	byte #<Rbitmap
+	byte #<Ubitmap
+	byte #<Dbitmap
+	byte #<NullBitmap
+	byte #<Xbitmap
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;  end ROM lookup tables
